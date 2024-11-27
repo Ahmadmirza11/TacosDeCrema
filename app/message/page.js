379185ca684f989d9c31020a5d1a32657e8a7384
Ahmed <ahmedmirza1255@gmail.com>
2024-11-27 11:08:29 +0500

    Init

---
+++ app/message/page.js
@@ -0,0 +1,64 @@
+"use client";
+import style from "./signup.module.css";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "@/firebaseConfig";
+
+export default function Message() {
+
+
+  const [message, setMessage] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await addDoc(collection(db,'messages'),{
+
+       message:message,
+        timestamp:new Date(),
+      }) 
+      alert("message is being Alert" ) 
+        router.push("/")
+
+     
+      
+    } catch (error) {
+      console.log("error in this",error)
+      
+    }
+  
+  };
+
+  return (
+    <div className={style.container}>
+      <div className={style.login}>
+        <h1 className={style.heading}>Your Message</h1>
+    
+        
+        <div>
+          <form onSubmit={handleSubmit}>
+           
+        <div>
+          <p className={style.text}>Message</p>
+        </div>
+        <div>
+            <textarea
+              type="text"
+              className={style.form2}
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+              required
+              placeholder="Enter your Message"
+            ></textarea>
+            </div>
+          <div className={style.button}>
+            <button type="submit" className={style.Button}>Alert</button>
+          </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ app/subscribe/page.js
@@ -0,0 +1,215 @@
+"use client";
+import React, { useState } from "react";
+import pic from "../../public/tacos.jpg";
+import Image from "next/image";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "@/firebaseConfig";
+import { useRouter } from "next/navigation";
+
+const statesWithCities = [
+  { state: "Alabama", cities: ["Birmingham", "Montgomery", "Mobile"] },
+  { state: "Alaska", cities: ["Anchorage", "Juneau", "Fairbanks"] },
+  { state: "Arizona", cities: ["Phoenix", "Tucson", "Mesa"] },
+  { state: "Arkansas", cities: ["Little Rock", "Fort Smith", "Fayetteville"] },
+  {
+    state: "California",
+    cities: ["Los Angeles", "San Francisco", "San Diego"],
+  },
+  { state: "Colorado", cities: ["Denver", "Colorado Springs", "Aurora"] },
+  { state: "Connecticut", cities: ["Hartford", "New Haven", "Stamford"] },
+  { state: "Delaware", cities: ["Wilmington", "Dover", "Newark"] },
+  { state: "Florida", cities: ["Miami", "Orlando", "Tampa"] },
+  { state: "Georgia", cities: ["Atlanta", "Savannah", "Augusta"] },
+  { state: "Hawaii", cities: ["Honolulu", "Hilo", "Kailua"] },
+  { state: "Idaho", cities: ["Boise", "Idaho Falls", "Pocatello"] },
+  { state: "Illinois", cities: ["Chicago", "Aurora", "Naperville"] },
+  { state: "Indiana", cities: ["Indianapolis", "Fort Wayne", "Evansville"] },
+  { state: "Iowa", cities: ["Des Moines", "Cedar Rapids", "Davenport"] },
+  { state: "Kansas", cities: ["Wichita", "Overland Park", "Kansas City"] },
+  { state: "Kentucky", cities: ["Louisville", "Lexington", "Bowling Green"] },
+  { state: "Louisiana", cities: ["New Orleans", "Baton Rouge", "Shreveport"] },
+  { state: "Maine", cities: ["Portland", "Lewiston", "Bangor"] },
+  { state: "Maryland", cities: ["Baltimore", "Annapolis", "Rockville"] },
+  { state: "Massachusetts", cities: ["Boston", "Worcester", "Springfield"] },
+  { state: "Michigan", cities: ["Detroit", "Grand Rapids", "Lansing"] },
+  { state: "Minnesota", cities: ["Minneapolis", "Saint Paul", "Rochester"] },
+  { state: "Mississippi", cities: ["Jackson", "Gulfport", "Biloxi"] },
+  { state: "Missouri", cities: ["Kansas City", "St. Louis", "Springfield"] },
+  { state: "Montana", cities: ["Billings", "Missoula", "Great Falls"] },
+  { state: "Nebraska", cities: ["Omaha", "Lincoln", "Bellevue"] },
+  { state: "Nevada", cities: ["Las Vegas", "Reno", "Henderson"] },
+  { state: "New Hampshire", cities: ["Manchester", "Nashua", "Concord"] },
+  { state: "New Jersey", cities: ["Newark", "Jersey City", "Paterson"] },
+  { state: "New Mexico", cities: ["Albuquerque", "Santa Fe", "Las Cruces"] },
+  { state: "New York", cities: ["New York City", "Buffalo", "Rochester"] },
+  { state: "North Carolina", cities: ["Charlotte", "Raleigh", "Greensboro"] },
+  { state: "North Dakota", cities: ["Fargo", "Bismarck", "Grand Forks"] },
+  { state: "Ohio", cities: ["Columbus", "Cleveland", "Cincinnati"] },
+  { state: "Oklahoma", cities: ["Oklahoma City", "Tulsa", "Norman"] },
+  { state: "Oregon", cities: ["Portland", "Salem", "Eugene"] },
+  {
+    state: "Pennsylvania",
+    cities: ["Philadelphia", "Pittsburgh", "Harrisburg"],
+  },
+  { state: "Rhode Island", cities: ["Providence", "Newport", "Warwick"] },
+  { state: "South Carolina", cities: ["Charleston", "Columbia", "Greenville"] },
+  { state: "South Dakota", cities: ["Sioux Falls", "Rapid City", "Aberdeen"] },
+  { state: "Tennessee", cities: ["Nashville", "Memphis", "Knoxville"] },
+  { state: "Texas", cities: ["Houston", "Dallas", "Austin"] },
+  { state: "Utah", cities: ["Salt Lake City", "Provo", "West Jordan"] },
+  { state: "Vermont", cities: ["Burlington", "Montpelier", "Rutland"] },
+  { state: "Virginia", cities: ["Richmond", "Virginia Beach", "Norfolk"] },
+  { state: "Washington", cities: ["Seattle", "Spokane", "Tacoma"] },
+  {
+    state: "West Virginia",
+    cities: ["Charleston", "Huntington", "Morgantown"],
+  },
+  { state: "Wisconsin", cities: ["Milwaukee", "Madison", "Green Bay"] },
+  { state: "Wyoming", cities: ["Cheyenne", "Casper", "Laramie"] },
+];
+
+const Subscribe = () => {
+  const [phone, setPhone] = useState("");
+  const [countryCode, setCountryCode] = useState("+1");
+  const [selectedState, setSelectedState] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    // Validate phone number length
+    if (phone.length !== 10) {
+      alert("Phone number must be exactly 10 digits");
+      setLoading(false);
+      return;
+    }
+    try {
+      await addDoc(collection(db, "Subscription"), {
+        phone: phone,
+        selectedState: selectedState,
+        selectedCity: selectedCity,
+      }); // Reset all fields to their initial state
+      setPhone("");
+      setSelectedState("");
+      setSelectedCity("");
+      alert("Your data is being updated")
+      router.push("message");
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+  // Determine if the form is complete
+  const isFormComplete =
+    phone && selectedState && selectedCity && phone.length == 10;
+
+  return (
+    <div className="w-screen h-screen relative flex justify-center items-center">
+      <div className="relative h-full w-full">
+        <Image
+          src={pic}
+          layout="fill"
+          objectFit="cover"
+          objectPosition="center"
+          alt="tacos"
+          className="blur-sm"
+        />
+      </div>
+      <div className="absolute flex flex-col justify-center items-center bg-transparent backdrop-blur-md border rounded-lg p-6 m-10 max-w-sm w-full">
+        <h2 className="text-center text-white font-bold text-2xl mb-4">
+          Subscribe
+        </h2>
+        <form onSubmit={handleSubmit} className="w-full ">
+          <div className="mb-4 w-full">
+            <label
+              htmlFor="phone"
+              className="block text-white text-sm font-semibold mb-2"
+            >
+              Phone Number
+            </label>
+            <div className="flex">
+              <select
+                id="countryCode"
+                value={countryCode}
+                onChange={(e) => setCountryCode(e.target.value)}
+                className="bg-gray-200 border border-gray-300 text-gray-700 p-2 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-blue-400 cursor-pointer"
+              >
+                <option value="+1">+1 (USA)</option>
+              </select>
+              <input
+                type="tel"
+                id="phone"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+                maxLength={10}
+                className="block w-full bg-white border border-gray-300 text-gray-700 py-2 px-3 rounded-r-lg leading-tight focus:outline-none focus:ring-2 focus:ring-blue-400"
+                placeholder="Enter phone number"
+              />
+            </div>
+          </div>
+          {/* states */}
+          <div className="mb-4 w-full">
+            <label
+              htmlFor="state"
+              className="block text-white text-sm font-semibold mb-2"
+            >
+              State
+            </label>
+            <select
+              id="state"
+              value={selectedState}
+              onChange={(e) => setSelectedState(e.target.value)}
+              className="block w-full bg-white border border-gray-300 text-gray-700 py-2 px-3 rounded-lg leading-tight focus:outline-none focus:ring-2 focus:ring-blue-400"
+            >
+              <option value="">Select State</option>
+              {statesWithCities.map(({ state }) => (
+                <option key={state} value={state}>
+                  {state}
+                </option>
+              ))}
+            </select>
+          </div>
+          {/* cities */}
+          <div className="mb-4 w-full">
+            <label
+              htmlFor="city"
+              className="block text-white text-sm font-semibold mb-2"
+            >
+              City
+            </label>
+            <select
+              id="city"
+              value={selectedCity}
+              onChange={(e) => setSelectedCity(e.target.value)}
+              className="block w-full bg-white border border-gray-300 text-gray-700 py-2 px-3 rounded-lg leading-tight focus:outline-none focus:ring-2 focus:ring-blue-400"
+            >
+              <option value="">Select City</option>
+              {selectedState &&
+                statesWithCities
+                  .find((state) => state.state === selectedState)
+                  .cities.map((city) => (
+                    <option key={city} value={city}>
+                      {city}
+                    </option>
+                  ))}
+            </select>
+          </div>
+          <button
+            type="submit"
+            className={`bg-orange-500 hover:bg-red-700 text-white font-bold text-xl py-2 px-4 rounded-full focus:outline-none focus:ring-2 focus:ring-blue-400 w-full mt-10 ${
+              !isFormComplete ? "opacity-50 cursor-not-allowed" : ""
+            }`}
+            disabled={!isFormComplete || loading}
+          >
+            {loading ? "Submitting..." : "Submit"}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Subscribe;
